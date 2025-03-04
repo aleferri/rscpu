19aaa2a04c74a9562b9da75c6e440ec76b853942
Alessio Ferri <alessio.ferri.3012@gmail.com>
2025-03-04 01:45:12 +0100

    adjusted scope and goal, renamed the project

--- apps/globals.s
+++ apps/globals.s
@@ -1,41 +1,44 @@
 
 
-_ix0:   .dw 0
-_ix1:   .dw 0
-_ix2:   .dw 0
-_ix3:   .dw 0
+_ix0:                               .dw 0  ; index base 0 (caller saved)
+_ix1:                               .dw 0  ; index base 1 (caller saved)
+_ix2:                               .dw 0  ; index base 2 (caller saved)
+_ix3:                               .dw 0  ; index base 3 (caller saved)
 
-_arg0:  .dw 0
-_arg1:  .dw 0
-_arg2:  .dw 0
-_arg3:  .dw 0
+_arg0:                              .dw 0  ; call argument 0
+_arg1:                              .dw 0  ; call argument 1
+_arg2:                              .dw 0  ; call argument 2
+_arg3:                              .dw 0  ; call argument 3
 
-_g0:    .dw 0
-_g1:    .dw 0
-_g2:    .dw 0
-_g3:    .dw 0
+_g0:                                .dw 0  ; global location 0 (caller saved)
+_g1:                                .dw 0  ; global location 1 (caller saved)
+_g2:                                .dw 0  ; global location 2 (caller saved)
+_g3:                                .dw 0  ; global location 3 (called saved)
 
-_ra:    .dw 0
-_sp:    .dw 0
+_b0:                                .dw 0  ; temp 0 (callee saved)
+_b1:                                .dw 0  ; temp 1 (callee saved)
+_b2:                                .dw 0  ; temp 2 (callee saved)
+_b3:                                .dw 0  ; temp 3 (callee saved)  
+
+_ra:                                .dw 0  ; return address, used in pseudo instruction RET alias "JPI _ra"
+_sp:                                .dw 0  ; stack pointer
 
 
-_const_1: .dw 1
-_const_2: .dw 2
-_const_4: .dw 4
-_const_jmp: .dw 0x8000
+_const_0001:                        .dw 1
+_const_0002:                        .dw 2
+_const_0004:                        .dw 4
+_const_000B:                        .dw 11
+_const_00FF:                        .dw 0xFF
+_const_8000:                        .dw 0x8000
 
-_protected:     JMP __t_error
-                JMP __t_error
-_trampoline:    .dw 0
-                JMP __t_error
-                JMP __t_error
                 
-_ioptr:     .dw 0xFFF0
+_ioptr:                             .dw 0xFFF0  ; ioptr base
+_errno:                             .dw 0
 
-_pow2_units: .dw 1, 2, 4, 8, 6, 2, 4
-_pow2_tens: .dw 0, 0, 0, 0, 1, 3, 6
+_pow2_units:                        .dw 1, 2, 4, 8, 6, 2, 4     ; nibble to units
+_pow2_tens:                         .dw 0, 0, 0, 0, 1, 3, 6     ; nibble to tens (0x0-0xA: 0, 0xB-0xF: nibble - 0xA)
     
 
 
-__t_error:  LDI 0
-            JMP __t_error
+__fatal_error:                      STA __errno
+                                    JMP __fatal_error
--- apps/lib.s
+++ apps/lib.s
@@ -1,87 +1,71 @@
 
-__return:       LIT     0
-                ADD     _const_jmp
-                ADD     _ra
-                STA     _trampoline
-                JMP     _trampoline
-
 ; memcopy: src, dest, n
-__memcopy:      LIT     0
-                ADD     _arg2
-                JZE     __return
-.loop:          LDI     _arg0
+__memcopy:      LDA     _arg2
+                JZE     .end
+.loop:          STA     _arg2
+                LDI     _arg0
                 STI     _arg1
                 INC     _arg0
                 INC     _arg1
-                LIT     0
-                ADD     _arg2
-                SUB     _const_1
-                STA     _arg2
-                JZE     __return
-                JMP     .loop
+                LDA     _arg2
+                SUB     _const_0001
+                JNZ     .loop
+.end:           JPI     _ra
                 
                 
-; memset: v, dest, n
-__memset:       LIT     0
-                ADD     _arg2
-                JZE     __return
-.loop:          LIT     0
-                ADD     _arg0
+; memset: c: int, dest: ptr, n: int
+__memset:       LDA     _arg2
+                JZE     .end
+.loop:          STA     _arg2
+                LDA     _arg0
                 STI     _arg1
                 INC     _arg1
-                LIT     0
-                ADD     _arg2
-                SUB     _const_1
-                STA     _arg2
-                JZE     __return
-                JMP     .loop
+                LDA     _arg2
+                SUB     _const_0001
+                JNZ     .loop
+.end:           JPI     _ra
                 
-                
+
+; split binary number in tens and units
+; bin2str: bin: char, dest: ptr
 __split_bin:    LIT     0x7F
                 AND     _arg0
                 STA     _arg0
-                LIT     _pow2_units
+                LIT     _pow2_units         ; prepare units table
                 STA     _ix0
-                LIT     _pow2_tens
+                LIT     _pow2_tens          ; prepare tens table
                 STA     _ix1
                 LIT     0
                 STA     _g0                 ; units
                 STA     _g1                 ; tens
-.loop:          LIT     1
-                AND     _arg0
-                JZE     .skip
-                LDI     _ix0
-                ADD     _g0
+.loop:          LDA     _arg0
+                AND     _const_0001         ; check if n & 1 is true, 
+                JZE     .skip               ; otherwise skip to the next bit
+                LDI     _ix0                ; read units table entry at [ix0]
+                ADD     _g0                 ; add to the units
                 STA     _g0
-                LDI     _ix1
-                ADD     _g1
+                LDI     _ix1                ; read tens table entry at [ix1]
+                ADD     _g1                 ; add to the tens
                 STA     _g1
-.skip:          INC    _ix0
-                INC    _ix1
-                SHR    _arg0
-                LIT    0
-                ADD    _arg0
-                JZE    .adjust
-                JMP    .loop
-.adjust:        LIT    10
-                STA    _g3
-                LIT    0
-                ADD    _g0
-                AND    _const_jmp
-                JZE    .skip2
-                JMP    .print
-.skip2:         LIT    0
-                ADD    _g0
+.skip:          INC    _ix0                 ; advance units table pointer
+                INC    _ix1                 ; advance tens table pointer
+                SHR    _arg0                ; shift right n
+                LDA    _arg0
+                JNZ    .loop                ; test not zero
+.adjust_test:   LDA    _g0
+                SUB    _const_000B          ; subtract 11 to units
+                AND    _const_8000          ; test sign bit
+                JNZ    .write               ; no overflow, skip adjustement
+.adjust:        LDA    _g0
                 SUB    _g3
                 STA    _g0
                 INC    _g1
-.print:         LIT    0
-                ADD    _g1
+                JMP    .adjust_test
+.write:         LDA    _g1                  ; write tens
+                STI    _arg1
+                INC    _arg1                ; increment pointer
+                LDA    _g0                  ; write units
                 STI    _arg1
                 INC    _arg1
-                LIT    0
-                ADD    _g0
-                STI    _arg1
-                INC    _arg1
-                JMP    __return
+                JPI    _ra                  ; return
                 
